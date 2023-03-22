84571f80e13d4dfe3dc605c01d84397862007a84
Aiden <54572444+AbyssWalkerG@users.noreply.github.com>
2023-03-22 00:37:58 -0400

    Add files via upload

--- a2.cpp
+++ a2.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 struct Invertedindex {
 	unordered_set<string> dictionary;
 	unordered_map<string, int> freq;
@@ -10,11 +12,29 @@ struct Invertedindex {
 
 int readStopWords();
 int isStopWord();
-int initIndexer(const string &mata_data, const string &index_name);
+int initIndex();
+int search();
 
 int main() {
     char *dbname = "test";
     Xapian::WritableDatabase db(dbname, Xapian::DB_CREATE_OR_OPEN);
     std::cout << "Hello, World" << std::endl;
+    initIndex();
     return 0;
 }
+
+int initIndex()
+{
+    // timestamp
+    auto begin = chrono::high_resolution_clock::now();
+    
+    // timestamp
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - begin);
+    
+    // output statistics
+    cout << "TIME: " << duration.count() << " milliseconds" << endl;
+    return 0;
+}
+
+
